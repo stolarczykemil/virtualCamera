ff2101e4f2e323008d2bd42efac0538200034eb4
stolarczykemil <stolarczykemil45@gmail.com>
2026-04-14 20:22:41 +0200

    scene definition in textfile and file reading functions

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-a
+import numpy as np
+
+def load_vertices(filename):
+    with open(filename, "r") as f:
+        vertices = []
+        for line in f:
+            points = line.split()
+            if len(points) == 3:
+                vertices.append([float(points[0]), float(points[1]), float(points[2])])
+    return np.array(vertices)
+
+def load_edges(filename):
+    with open(filename, "r") as f:
+        edges = []
+        for line in f:
+            parts = line.split()
+            if len(parts) == 2:
+                edges.append([int(parts[0]), int(parts[1])])
+    return edges
+
